acb265857d0e0d39287defa58a0eb4574115a157
Nicolas Seriot <nicolas@seriot.ch>
2009-12-05 06:00:43 +0100

    fixed crash with big photo libraries

--- main.m
+++ main.m
@@ -3,7 +3,7 @@
 //  SpyPhone
 //
 //  Created by Nicolas Seriot on 11/15/09.
-//  Copyright IICT 2009. All rights reserved.
+//  Copyright 2009. 
 //  Licensed under GPL 2.0 http://www.gnu.org/licenses/gpl-2.0.txt
 //
 
